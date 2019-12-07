01cf6d3061c949b63df11eaba7c173f154e7f593
JackMaDrinkCoffee <596770421@qq.com>
2019-12-07 15:53:44 +0800

    +init

---
+++ Source/PluginDevelop/PluginDevelop.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PluginDevelop.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PluginDevelop, "PluginDevelop" );
